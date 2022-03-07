9da9b8d563a73397dd01c1e91a9b8b80c84b1dbb
cahito <cahito@gmail.com>
2022-03-07 16:49:56 -0300

    Criado NavBar temporario

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
 
 class Favorites extends React.Component {
   render() {
     return (
       <div data-testid="page-favorites">
-        <p>Header</p>
-        <p>NavBar</p>
+        <Header />
+        <NavBar />
         <p>Músicas favoritas</p>
       </div>
     );
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
 
 class Profile extends React.Component {
   render() {
     return (
       <div data-testid="page-profile">
-        <p>Header</p>
-        <p>NavBar</p>
+        <Header />
+        <NavBar />
         <p>Perfil do usuário</p>
       </div>
     );
